5cef1022850cd7811c0ec5390ab72235605cce03
Gabriel <jgbiel29@gmail.com>
2024-10-14 11:46:32 -0300

    Turns out Ec2 was not responding because I forgot to put the docker port to 443. Reverted back to the original uwsgi command and changed the port in docker-compose.yml.

--- app/run.sh
+++ app/run.sh
@@ -4,4 +4,4 @@ set -e
 
 openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 3650 -nodes -subj "/C=XX/ST=StateName/L=CityName/O=CompanyName/OU=CompanySectionName/CN=CommonNameOrHostname"
 
-uwsgi --https 0.0.0.0:443,cert.pem,key.pem --workers 4 --master --enable-threads --module app:app 
+uwsgi --https 0.0.0.0:5000,cert.pem,key.pem --workers 4 --master --enable-threads --module app:app 
